9dd4f0f6e2307ba9f7442b711ba7a38a3587231d
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2022-06-25 19:17:56 +0300

    implements currying function

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import './styles/style.scss';
 // import dImage from './assets/dImage.png';
 
 
-const welcome = (value) => {
-    return (type) => {
+const welcome = (value) => (type) => {
         let body = document.getElementsByTagName('body')[0];
         let element = document.createElement(`${type}`);
         element.textContent = value;
         body.appendChild(element);
         return body;
-    };
 }
 
 welcome('understand-redux-with-webpack')('h1');
