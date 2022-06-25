f38369426a4fd8b1aff699408907fe1702b7549a
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2022-06-25 22:17:17 +0300

    implement spread-operator with arrays

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 
-const person = {name: 'Dominic'};
-const updatedPerson = Object.assign({}, person, {name: 'Emmanuel'});
-console.log(updatedPerson);
+const numbers = [1, 2, 3]
 
-const user = {name: 'Matembu'};
-// since spread operator ...person has a shallow copy at this point, we strongly override
-// the name field with a string Emmanuel
-const updatedUser = {...person, name: 'Emmanuel'};
-console.log(updatedUser);
-
-const worker = {
-        name: 'Matembu',
-        occupation: 'Software Engineer'
-    };
-const updatedWorker = {...person, name: 'Emmanuel'};
-// This returns occupation with DevOps Engineer because the
-// spread operator ...person has a shallow copy at this point from the reference
-worker.occupation = 'DevOps Engineer';
-console.log(worker);
-
+const added1 = [...numbers, 4];
+const added2 = [0, ...numbers];
+const index = numbers.indexOf(2);
+const added3 = [
+        ...numbers.slice(0, index),
+        5,
+        ...numbers.slice(index)
+        ];
+const removed = added3.filter(n => n !== 5);
+const updated = numbers.map(n => n === 3 ? 30 : n);
+console.log(added1);
+console.log(added2);
+console.log(added3);
+console.log(removed);
+console.log(updated);
