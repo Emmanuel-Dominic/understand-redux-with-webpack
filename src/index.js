b677e86f2f82be0c6d01e2794091d6c59c976cff
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2022-06-25 22:36:02 +0300

    implements immutablity with immer

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
+import { produce } from 'immer';
 
-const numbers = [1, 2, 3]
+let book = {title: 'Harry Potter'};
 
-const added1 = [...numbers, 4];
-const added2 = [0, ...numbers];
-const index = numbers.indexOf(2);
-const added3 = [
-        ...numbers.slice(0, index),
-        5,
-        ...numbers.slice(index)
-        ];
-const removed = added3.filter(n => n !== 5);
-const updated = numbers.map(n => n === 3 ? 30 : n);
-console.log(added1);
-console.log(added2);
-console.log(added3);
-console.log(removed);
-console.log(updated);
+const publish = book => {
+    return produce(book, draftBook => {
+        draftBook.isPublished = true;
+    })
+}
+
+let updated = publish(book);
+console.log(book)
+console.log(updated)
