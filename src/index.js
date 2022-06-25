548dee55dd744dbc11c8734ff952620184414efd
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2022-06-25 22:01:26 +0300

    implements immutablity using a spread-operator

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import { compose, pipe } from 'lodash/fp';
-import './styles/style.scss';
-// import dImage from './assets/dImage.png';
 
+const person = {name: 'Dominic'};
+const updatedPerson = Object.assign({}, person, {name: 'Emmanuel'});
+console.log(updatedPerson);
 
-const welcome = (value) => (type) => {
-        let body = document.getElementsByTagName('body')[0];
-        let element = document.createElement(`${type}`);
-        element.textContent = value;
-        body.appendChild(element);
-        return body;
-}
+const user = {name: 'Matembu'};
+// since spread operator ...person has a shallow copy at this point, we strongly override
+// the name field with a string Emmanuel
+const updatedUser = {...person, name: 'Emmanuel'};
+console.log(updatedUser);
 
-welcome('understand-redux-with-webpack')('h1');
-// welcome('understand-redux-with-webpack');
-// welcome('h1');
+const worker = {
+        name: 'Matembu',
+        occupation: 'Software Engineer'
+    };
+const updatedWorker = {...person, name: 'Emmanuel'};
+// This returns occupation with DevOps Engineer because the
+// spread operator ...person has a shallow copy at this point from the reference
+worker.occupation = 'DevOps Engineer';
+console.log(worker);
+
