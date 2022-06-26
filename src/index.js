5283c64e00ff8f5056fa273fc190e76de3af845a
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2022-06-26 09:15:47 +0300

    implements action creators

--- src/index.js
+++ src/index.js
@@ -1,28 +1,7 @@
 import store from './store';
-import * as actions from './actionTypes';
+import { addBug, removeBug } from './actions';
 
-console.log(store);
-
-const unsubscribe = store.subscribe(() => {
-    console.log('Store changed!', store.getState());
-});
-
-console.log(store.getState(), 'initialState');
-
-store.dispatch({
-    type: actions.ADD_BUG,
-    payload: {
-        description: 'This is our first Bug.'
-    }
-});
-
-unsubscribe();
-
-store.dispatch({
-    type: actions.REMOVE_BUG,
-    payload: {
-        id: 1
-    }
-});
-
+store.dispatch(addBug('This is our updated Bug.'));
+console.log(store.getState());
+store.dispatch(removeBug(1));
 console.log(store.getState());
